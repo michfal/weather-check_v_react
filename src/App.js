e5d8ef70fde7c8e0e91b98e3d06e8e88384aef07
Michał Fal <michfal86@gmail.com>
2021-04-17 15:09:00 +0200

    added class to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
+import MainPage from './pages/MainPage/MainPage.components';
+
 import './App.scss';
 
-import MainPage from './pages/MainPage/MainPage.components'
 
-function App() {
-  return (
-    <div className="App">
-        <MainPage />   
-    </div>
-  );
+
+// function App() {
+//   return (
+//     <div className="App">
+//         <MainPage />   
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainPage />
+    );
+  }
 }
 
 export default App;
