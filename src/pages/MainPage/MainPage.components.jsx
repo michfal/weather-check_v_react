21c4721d28152eb41a3ccbd3e36fcff5e6bb9eda
Michał Fal <michfal86@gmail.com>
2021-06-18 15:52:08 +0200

    fixed main page reload bug. Now after reloading turns to intro page

--- src/pages/MainPage/MainPage.components.jsx
+++ src/pages/MainPage/MainPage.components.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import WeatherDataContext from '../../context/WeatherDataContext';
+import { useHistory } from "react-router";
 import CurrentInfo from '../../components/CurrentInfo/CurrentInfo.components';
 import DailyInfo from '../../components/DailyInfo/DailyInfo.components';
 
 function CurrentDayPage() {
+  const {state} = useContext(WeatherDataContext);
+  const history = useHistory();
+  if(state.cityName !== '') {
     return (
-      <React.Fragment >
+      <React.Fragment  >
         <CurrentInfo />
         <DailyInfo />
       </React.Fragment>
     )
-  } 
+  };
+  
+  history.push("/");
+  return '';
+};
 
-  export default CurrentDayPage;
+export default CurrentDayPage;
